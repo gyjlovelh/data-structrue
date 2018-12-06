6a4df765260f41324406625b495ad7ec53850bb5
gyjlovelh <18062791691@163.com>
2018-12-06 16:56:59 +0800

    binary-tree

--- performance/performance.ts
+++ performance/performance.ts
@@ -7,10 +7,10 @@ export const performance = function () {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         const oldMethod = descriptor.value;
         descriptor.value = function (...arg) {
-            const begin = Date.now();
-            const result = oldMethod.apply(this, arg);
-            const end = Date.now();
-            console.log(`【${propertyKey}】方式执行完成耗时：${end - begin}ms`);
+            const begin = Date.now(),
+                result = oldMethod.apply(this, arg),
+                end = Date.now();
+            window.console.warn(`【${propertyKey}】函数执行完成共耗时：${end - begin}ms`);
             return result;
         };
     };
