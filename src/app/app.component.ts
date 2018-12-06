5ed6cd19681ca471932d069a83a968b0feb2bc6e
gyjlovelh <18062791691@163.com>
2018-12-06 10:17:58 +0800

    binary-tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {BinaryTree} from '../../structrue/binary-tree';
 
 @Component({
-  selector: 'hs-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'hs-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'data-structure';
+    title = 'data-structure';
+
+    constructor() {
+        const arr = [];
+        for (let i = 0; i < 1000; i++) {
+            arr.push({
+                id: this.getUUID(),
+                name: `name${i}`
+            });
+        }
+
+        const bi = new BinaryTree(arr, 'id');
+        console.log('BinaryTree:', bi);
+    }
+
+    getUUID(len: number = 36) {
+        const uuid = [];
+        const str = '0123456789abcdef';
+        for (let i = 0; i < len; i++) {
+            uuid[i] = str.substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        if (len === 36) {
+            uuid[14] = '4';
+            uuid[19] = str.substr((uuid[19] & 0x3 | 0x8), 1)
+            uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+        }
+        return uuid.join('').replace(/-/g, '');
+    }
 }
---
+++ structrue/binary-tree.ts
@@ -0,0 +1,94 @@
+/**
+ * Created by guanyj on  12/6/18
+ */
+
+export class BinaryTree<T> {
+
+    /**
+     * 二叉树根元素
+     */
+    private root: BinaryTreeItem;
+
+    constructor(
+        public array: Array<T>,
+        public binaryKey: string = '$binaryId'
+    ) {
+        array.forEach(item => {
+            this.insert(item);
+        });
+    }
+
+    /**
+     * insert into binaryTree normally
+     *
+     * i:Having all the properties of a two - fork sort tree;
+     * ii:the absolute value of the depth difference between the left subtree and the right subtree is not more than 1.
+     * iii:the left and right subtrees are both the two fork balance trees.
+     *
+     * @param element
+     */
+    insert(element: T): void {
+        const item = new BinaryTreeItem(element[this.binaryKey], element);
+        if (this.root) {
+            (function _insertNode(node: BinaryTreeItem, newNode: BinaryTreeItem) {
+                if (node.value > newNode.value) {
+                    if (node.left !== null) {
+                        _insertNode(node.left, newNode);
+                    } else {
+                        node.left = newNode;
+                    }
+                } else {
+                    if (node.right !== null) {
+                        _insertNode(node.right, newNode);
+                    } else {
+                        node.right = newNode;
+                    }
+                }
+            })(this.root, item);
+        } else {
+            this.root = item;
+        }
+    }
+
+    /**
+     * 查找指定节点
+     * @param {string | number} binaryId
+     * @returns {T}
+     */
+    find(binaryId: string | number): T {
+        let result = null;
+        if (this.root) {
+            (function _find(node: BinaryTreeItem, key: string | number) {
+                if (node.value === key) {
+                    result = node.element;
+                } else if (node.value > key) {
+                    if (node.left !== null) {
+                        _find(node.left, key);
+                    }
+                } else if (node.value < key) {
+                    if (node.right !== null) {
+                        _find(node.right, key);
+                    }
+                } else {
+                    result = null;
+                }
+            })(this.root, binaryId);
+        }
+        return result;
+    }
+}
+
+export class BinaryTreeItem {
+    value: string;
+    element: any;
+    left: BinaryTreeItem;
+    right: BinaryTreeItem;
+
+    constructor(value, element) {
+        this.value = value;
+        this.element = element;
+        this.left = null;
+        this.right = null;
+    }
+}
+
